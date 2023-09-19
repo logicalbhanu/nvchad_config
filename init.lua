f241543c7a44dd2d94ce08f979e23503f24c8386
Bhanu Pratap Mishra <bhanu@machine.local>
2023-09-19 21:44:20 +0530

    setup python provider
    Note that by-default in nvchad the python provider has been disabled and to enable
    it i have change the value of vim.g.loaded_python3_provider=1 in the nvchad project
    config(not my custom config), though i do have put this in my custom config also
    but that is not working.

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
--- local autocmd = vim.api.nvim_create_autocmd
-
+-- local autocmd = vim.api.nvim_create_autormd
+-- vim.g.loaded_python3_provider = 1
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
 --   pattern = "*",
@@ -9,3 +9,5 @@
 --
 -- setting custom vim options
 vim.opt.rnu = true --enable relative line-number
+-- Set the path for the Python 3 interpreter
+vim.g.python3_host_prog = "/Users/bhanu/.local/share/virtualenvs/nvim_env-wEheGUQG/bin/python"
