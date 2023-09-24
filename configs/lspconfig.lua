737168610b7398e9187bc4312744d6835a5bbaea
Bhanu Pratap Mishra <bhanu@machine.local>
2023-09-24 16:59:10 +0530

    configure the pylsp to use mypy as type checker, jedi completion and isort autoimport
    some other tweaking has also been done to improve the work flow.

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -24,16 +24,16 @@ end
 --   capabilities = capabilities,
 -- }
 
---  ############################################## Configuring python lsp ##################################################    
---  #################################################################################################### 
---  #################################################################################################### 
---  #################################################################################################### 
+--  ############################################## Configuring python lsp ##################################################
+--  ####################################################################################################
+--  ####################################################################################################
+--  ####################################################################################################
 
 -- lspconfig.pyright.setup { blabla}
 
 -- setting up pylsp
 local utils = require "core.utils"
-on_attach = function(client, bufnr)
+local custom_attach = function(client, bufnr)
   client.server_capabilities.documentFormattingProvider = true
   client.server_capabilities.documentRangeFormattingProvider = true
 
@@ -48,42 +48,71 @@ on_attach = function(client, bufnr)
   end
 end
 
+-- to enable the capabilities via nvim-cmp use this
+-- note that only one can be enabled with the default
+-- by vim or the one by cmp, and i found nvim-cmp capabilities
+-- inferior to the default one of vim.
+--M.capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 lspconfig.pylsp.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
+  on_attach = custom_attach,
   settings = {
     pylsp = {
       plugins = {
-        -- disabling pycodestyle to use flake8
-        pycodestyle = {
-          enabled = false,
-          ignore = { "W391" },
-          maxLineLength = 100,
-        },
-        jedi_hover = { enabled = true },
-        --jedi_completion = {eager = true},
-        rope_autoimport = { enabled = true },
-        mccabe = { enabled = false },
-        pyflakes = { enabled = false },
-        flake8 = { enabled = false, maxLineLength = 100 },
+        -- formatter options
         black = { enabled = true },
+        autopep8 = { enabled = false },
+        yapf = { enabled = false },
+        -- linter options
         ruff = {
           enabled = true,
-          --select = { "ALL" },      -- this is to select the rules that we want to include
-                                     -- in the diagnostics for the ruff lsp, similarly for format.
+          --select = { "ALL" },
+          -- this is to select the rules that we want to include
+          -- in the diagnostics for the ruff lsp, similarly for format.
           format = { "ALL" },
           extendSelect = { "I" },
         },
+        pylint = { enabled = false, executable = "pylint" },
+        pyflakes = { enabled = false },
+        pycodestyle = { enabled = false },
+        -- type checker
+        pylsp_mypy = { enabled = true },
+        -- auto-completion options
+        jedi_completion = { fuzzy = true },
+        -- import sorting
+        pyls_isort = { enabled = true },
       },
     },
   },
+  flags = {
+    debounce_text_changes = 200,
+  },
+  capabilities = capabilities,
 }
 
 -- setting up ruff for python will experiment it in future as not getting hover in this way
--- future approach is to use ruff from here and 
+-- future approach is to use ruff from here and
 -- Configure `ruff-lsp`.
 -- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#ruff_lsp
 -- For the default config, along with instructions on how to customize the settings
+--
+-- on_attach = function(client, bufnr)
+--   client.server_capabilities.documentFormattingProvider = true
+--   client.server_capabilities.documentRangeFormattingProvider = true
+--   -- disabling it in favor of pylsp
+--   client.server_capabilities.hoverProvider = true
+--
+--   utils.load_mappings("lspconfig", { buffer = bufnr })
+--
+--   if client.server_capabilities.signatureHelpProvider then
+--     require("nvchad.signature").setup(client)
+--   end
+--
+--   if not utils.load_config().ui.lsp_semantic_tokens and client.supports_method "textDocument/semanticTokens" then
+--     client.server_capabilities.semanticTokensProvider = nil
+--   end
+-- end
+--
 -- lspconfig.ruff_lsp.setup {
 --   on_attach = on_attach,
 --   capabilities = capabilities,
